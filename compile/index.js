6118f8a76e581bc59fb176af964b4c88d7a4000a
Bill Ticehurst <billti@hotmail.com>
2023-08-23 00:47:48 -0700

    More error handling

--- compile/index.js
+++ compile/index.js
@@ -5,95 +5,110 @@
 
 // For developing functions, see https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference-node
 
-const util = require('util')
-const fs   = require('fs/promises');
+const util = require("util");
+const fs = require("fs/promises");
 
-const join =        require('path').join;
-const tmpdir =      require('os').tmpdir;
-const randomBytes = require('crypto').randomBytes;
+const join = require("path").join;
+const tmpdir = require("os").tmpdir;
+const randomBytes = require("crypto").randomBytes;
 
-const exec = util.promisify(require('child_process').execFile);
+const exec = util.promisify(require("child_process").execFile);
 
 const binPath = join(__dirname, process.platform, process.arch);
 const qat = join(binPath, "qat");
 
 function getTempInputFilename() {
-    return join(tmpdir(), `qsc-${randomBytes(16).toString('hex')}.ll`);   
+  return join(tmpdir(), `qsc-${randomBytes(16).toString("hex")}.ll`);
 }
 function getTempOutputFilename() {
-    return join(tmpdir(), `qsc-${randomBytes(16).toString('hex')}.bc`);   
+  return join(tmpdir(), `qsc-${randomBytes(16).toString("hex")}.bc`);
 }
 
 /**
- * 
- * @param {import('@azure/functions').Context} context 
- * @param {import('@azure/functions').HttpRequest} req 
- * @returns 
+ *
+ * @param {import('@azure/functions').Context} context
+ * @param {import('@azure/functions').HttpRequest} req
+ * @returns
  */
 module.exports = async function (context, req) {
-    if (!req.body) {
-        context.res = {status: 400, body: "No source code provided"};
-        return;
-    }
-    context.log("Got a request with body length: " + req.bufferBody?.length);
+  if (!req.body) {
+    context.res = { status: 400, body: "No source code provided" };
+    return;
+  }
+  context.log("Got a request with body length: " + req.bufferBody?.length);
 
-    let target = "rigetti";
-    if (req.get("x-hardware-target") === "quantinuum") {
-        target = "quantinuum";
-    }
-    context.log("Setting hardware target to: " + target);
-    
-    const tmpInputFile = getTempInputFilename();
-    const tmpOutputFile = getTempOutputFilename();
-    const decompFile = join(binPath, (target === "rigetti" ? "decomp_b340.ll" : "decomp_7ee0.ll"));
+  let target = "rigetti";
+  if (req.get("x-hardware-target") === "quantinuum") {
+    target = "quantinuum";
+  }
+  context.log("Setting hardware target to: " + target);
 
-    if (!req.bufferBody) return; // TODO: Return 400 - Bad Request
-    await fs.writeFile(tmpInputFile, req.bufferBody);
-    context.log("Input file written to: " + tmpInputFile);
+  const tmpInputFile = getTempInputFilename();
+  const tmpOutputFile = getTempOutputFilename();
+  const decompFile = join(
+    binPath,
+    target === "rigetti" ? "decomp_b340.ll" : "decomp_7ee0.ll"
+  );
 
+  if (!req.bufferBody) return; // TODO: Return 400 - Bad Request
+  await fs.writeFile(tmpInputFile, req.bufferBody);
+  context.log("Input file written to: " + tmpInputFile);
+
+  try {
+    // Check the files exist as expected
+    const inputStat = await fs.stat(tmpInputFile);
+    context.log("Input file exists with size: " + inputStat.size);
+    const decompStat = await fs.stat(decompFile);
+    context.log("Decomp file exists with size: " + decompStat.size);
+
+    const args = [
+      "--apply",
+      "--always-inline",
+      "--no-disable-record-output-support",
+      "--entry-point-attr",
+      "entry_point",
+      "--output",
+      tmpOutputFile,
+      tmpInputFile,
+      decompFile,
+    ];
+    context.log("Running QAT with args: " + args.join(", "));
+    let succeeded = false;
+    let response = {};
     try {
-        // Check the files exist as expected
-        const inputStat = await fs.stat(tmpInputFile);
-        context.log("Input file exists with size: " + inputStat.size);
-        const decompStat = await fs.stat(decompFile);
-        context.log("Decomp file exists with size: " + decompStat.size);
-
-        const args = [
-          "--apply", "--always-inline", "--no-disable-record-output-support",
-          "--entry-point-attr", "entry_point",
-          "--output", tmpOutputFile,
-          tmpInputFile, decompFile
-        ];
-        context.log("Running QAT with args: " + args.join(", "));
-        const response = await exec(qat, args);
-        // const response = await exec(qat, ["--help"]);
-        if (response.stderr) context.log("QAT stderr: " + response.stderr);
-        context.log("QAT stdout: " + response.stdout);
-
-        const bitcode = await fs.readFile(tmpOutputFile);
-        // const bitcode = "testing";
-        context.log("output bitcode size: " + bitcode.byteLength);
-
-        /** @type {import('@azure/functions').HttpResponseSimple} */
-        const res = {
-            headers: {
-                "Content-Type": "application/octet-stream"
-            },
-            body: bitcode,
-        }
-        context.res = res;
-    } catch (err) {
-        context.res = {
-            status: 400,
-            body: JSON.stringify(err)
-        }
+      response = await exec(qat, args);
+      succeeded = true;
+    } catch (e) {
+      context.log("QAT failed with: " + e.toString());
+      context.res = { status: 500, body: "QAT failed with: " + e.toString() };
     }
-    try {
-        await fs.rm(tmpInputFile);
-        await fs.rm(tmpOutputFile);
-    } catch(e) {
-        context.log("Failed to clean up temporary files: " + e.toString());
-    }
+    if (succeeded) {
+      // const response = await exec(qat, ["--help"]);
+      if (response.stderr) context.log("QAT stderr: " + response.stderr);
+      context.log("QAT stdout: " + response.stdout);
 
-    context.log("All done.");
-}
+      const bitcode = await fs.readFile(tmpOutputFile);
+      context.log("output bitcode size: " + bitcode.byteLength);
+
+      context.res = {
+        headers: {
+          "Content-Type": "application/octet-stream",
+        },
+        body: bitcode,
+      };
+    }
+  } catch (err) {
+    context.res = {
+      status: 500,
+      body: JSON.stringify(err),
+    };
+  }
+  try {
+    await fs.rm(tmpInputFile);
+    await fs.rm(tmpOutputFile);
+  } catch (e) {
+    context.log("Failed to clean up temporary files: " + e.toString());
+  }
+
+  context.log("All done.");
+};
